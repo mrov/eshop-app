dcedb0e6e3019c678ff7b62e9ead564ad4609096
Moabe Renato <mrov@cin.ufpe.br>
2023-02-27 14:56:00 -0300

    feat: starting the cart view

--- src/views/Cart.tsx
+++ src/views/Cart.tsx
@@ -1,10 +1,15 @@
 import * as React from 'react';
 import {Text, View} from 'react-native';
+import {useCart} from '../shared/contexts/CartContext';
 
 function Cart(): JSX.Element {
+  const cartProducts = useCart();
+
   return (
-    <View>
-      <Text style={{color: 'black'}}>Cart World</Text>
+    <View style={{backgroundColor: 'white', marginHorizontal: 10}}>
+      <Text style={{color: 'black'}}>
+        {cartProducts.length} carros selecionados:
+      </Text>
     </View>
   );
 }
